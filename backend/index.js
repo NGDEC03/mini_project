f635897b466d1d779bc5ef6f97dbf3d9a56d5247
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-08-17 14:35:48 +0530

    add routes

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,19 @@
 // const express = require('express')
-import express from "express";
+import express from "express"; // method-2
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
+import userRoute from "./routes/userRoute.js";
 
 dotenv.config({});
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+// middleware
+
+app.use(express.json());
+//routes
+app.use("/api/v1/user", userRoute);
 
 app.listen(PORT, () => {
   connectDB();
