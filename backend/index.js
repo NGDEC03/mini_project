157100090e954f31e362c6625834fef033266ad0
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-08-18 15:20:16 +0530

    fetch the data from backend to the frontend

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import connectDB from "./config/database.js";
 import userRoute from "./routes/userRoute.js";
 import messageRoute from "./routes/messageRoute.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config({});
 
@@ -12,9 +13,15 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 // middleware
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+const corsOption = {
+  origin: "http://localhost:3001",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOption));
 //routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/message", messageRoute);
