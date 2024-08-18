8ff00f8f071239e5a12d3db36e6d8ca55cc23074
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-08-18 12:23:51 +0530

    design signup login page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
 
+
+
 const Login = () => {
+        const [user, setUser] = useState({
+          username: "",
+          password: "",
+        });
+        const onSubmitHandler = (e) => {
+          e.preventDefault();
+          console.log(user);
+          setUser({
+            username: "",
+            password: "",
+            });
+    }
+    
   return (
     <div className="min-w-96 mx-auto">
       <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100'>
         <h1 className='text-3xl font-bold text-center'>Login</h1>
-        <form  action="">
+        <form onSubmit={onSubmitHandler}  action="">
 
           <div>
             <label className='label p-2'>
               <span className='text-base label-text'>Username</span>
             </label>
             <input
+                value={user.username}
+                onChange={(e) => setUser({...user, username: e.target.value})}
               className='w-full input input-bordered h-10'
               type="text"
               placeholder='Username' />
@@ -22,6 +39,8 @@ const Login = () => {
               <span className='text-base label-text'>Password</span>
             </label>
             <input
+                value={user.password}
+                onChange={(e) => setUser({...user, password: e.target.value})}
               className='w-full input input-bordered h-10'
               type="password"
               placeholder='Password' />
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,20 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
 
 
 
+
 const Signup = () => {
+    const [user, setUser] = useState({
+        fullName: '',
+        username: '',
+        password: '',
+        confirmPassword: '',
+        gender: '',
+    });
+    const handleCheckbox = (gender) => {
+        setUser({ ...user, gender });
+      }
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault()
+        console.log(user);
+        setUser({
+            fullName: '',
+            username: '',
+            password: '',
+            confirmPassword: '',
+        })
+    }
   return (
     <div className="min-w-96 mx-auto">
       <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100'>
         <h1 className='text-3xl font-bold text-center'>Signup</h1>
-        <form  action="">
+        <form onSubmit={onSubmitHandler} action="">
           <div>
             <label className='label p-2'>
               <span className='text-base label-text'>Full Name</span>
             </label>
             <input
-              className='w-full input input-bordered h-10'
+            value={user.fullName}
+            onChange={(e) => setUser({...user, fullName: e.target.value})}
+        
+            className='w-full input input-bordered h-10'
               type="text"
               placeholder='Full Name' />
           </div>
@@ -23,6 +48,8 @@ const Signup = () => {
               <span className='text-base label-text'>Username</span>
             </label>
             <input
+            value={user.userame}
+          onChange={(e) => setUser({...user, username: e.target.value})}
               className='w-full input input-bordered h-10'
               type="text"
               placeholder='Username' />
@@ -32,6 +59,8 @@ const Signup = () => {
               <span className='text-base label-text'>Password</span>
             </label>
             <input
+            value={user.password}
+            onChange={(e) => setUser({...user, password: e.target.value})}
               className='w-full input input-bordered h-10'
               type="password"
               placeholder='Password' />
@@ -41,6 +70,8 @@ const Signup = () => {
               <span className='text-base label-text'>Confirm Password</span>
             </label>
             <input
+            value={user.confirmPassword}
+            onChange={(e) => setUser({...user, confirmPassword: e.target.value})}
               className='w-full input input-bordered h-10'
               type="password"
               placeholder='Confirm Password' />
@@ -50,6 +81,8 @@ const Signup = () => {
               <p>Male</p>
               <input
                 type="checkbox"
+                checked={user.gender === "male"}
+                onChange={() => handleCheckbox("male")}
                 defaultChecked
                 className="checkbox mx-2" />
             </div>
@@ -57,6 +90,8 @@ const Signup = () => {
               <p>Female</p>
               <input
                 type="checkbox"
+                checked={user.gender === "female"}
+                onChange={() => handleCheckbox("female")}
                 defaultChecked
                 className="checkbox mx-2" />
             </div>
